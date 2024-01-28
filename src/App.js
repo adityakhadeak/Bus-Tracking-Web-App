b3afc81e8fcc62d7ee03401c92f2c1838141148d
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-01-28 22:58:27 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home.js';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
-);
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route element={<Home />} path='/' />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'custom': ['Raleway','Ubuntu','Nunito Sans', 'sans-serif'],
+        'sans': ['Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
